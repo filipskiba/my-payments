68b4988aa774c666339e9631a35ac4b3181554d0
filipskiba <filipskiba.247@gmail.com>
2020-02-05 18:42:52 +0100

    Use port 80

--- src/main/java/com/mypayments/MyPaymentsApplication.java
+++ src/main/java/com/mypayments/MyPaymentsApplication.java
@@ -4,14 +4,21 @@ import com.mypayments.domain.BankAccount;
 import com.mypayments.domain.Contractor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import java.util.ArrayList;
 
 @SpringBootApplication
-public class MyPaymentsApplication {
+public class MyPaymentsApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(MyPaymentsApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MyPaymentsApplication.class);
     }
 
 }
