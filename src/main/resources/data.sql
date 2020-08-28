137f18b65cc868b6da4d312015ee0b3d35ff3122
filipskiba <filipskiba.247@gmail.com>
2020-08-28 12:48:21 +0200

    change properties

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
-replace into payments.contractor_type set contractortype_id=1, contractortype_name="właściciel";
-replace into payments.contractor_type set contractortype_id=2, contractortype_name="klient";
+insert into contractor_type set contractortype_id=1, contractortype_name="właściciel";
+insert into contractor_type set contractortype_id=2, contractortype_name="klient";
